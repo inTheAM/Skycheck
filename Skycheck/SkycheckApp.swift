785e1c7c2c1ed202155b3e82f9a6960b64fcbd74
Ahmed Mgua <mguaahmed@gmail.com>
2021-08-17 17:34:38 +0300

    Initial Commit

---
+++ Skycheck/SkycheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkycheckApp.swift
+//  Skycheck
+//
+//  Created by Ahmed Mgua on 17/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SkycheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
